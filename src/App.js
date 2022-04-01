bc5c4316218efff648f4f79c62a5da029839fd29
portal116 <tim950116@gmail.com>
2022-04-01 17:48:12 +0900

    0401

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { useState } from "react";
+// import React from "react";
+// import Counter from "./Counter";
+
+// const App = () => {
+//   return <Counter />;
+// };
+
+// export default App;
+
+import React from "react";
 import Info from "./Info";
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 };
 
 export default App;
+
+// import React, { useState } from "react";
+// import Info from "./Info";
+
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}
+//       >
+//         {visible ? "숨기기" : "보이기"}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import React from "react";
+// import ContextSample from "./ContextSample";
+
+// const App = () => {
+//   return <ContextSample />;
+// };
+
+// export default App;
